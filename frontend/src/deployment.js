89e2c19d45a90990d74c89f4c44fd031580a708d
Pranith Kallakuri <45889668+pranithkallakuri@users.noreply.github.com>
2021-04-29 01:15:12 +0530

    Delete frontend directory

--- frontend/src/deployment.js
+++
@@ -1,5 +0,0 @@
-const frontendUrl = "https://homechef-se.herokuapp.com";
-
-const  backendUrl = "https://shielded-retreat-07467.herokuapp.com"; 
-
-export default backendUrl ;
--- frontend/src/screens/ChefScreen.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Menu from "../components/menu";
-
-const ChefScreen = () => {
-    return <div>Hey Chef!</div>;
-    //return <Menu />;
-};
-
-export default ChefScreen;
